25f65267b5722a8049b56a04bca3df1b4d9bc0a3
SebastianMaldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2022-04-20 16:16:12 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -13,7 +13,7 @@ class Cliente:
     
 class Modulo(ABC):
   @abstractmethod
-  def busqueda(self):
+  def busqueda(self, base_datos, nombre):
     pass
     
 class Productos(ABC):
@@ -37,4 +37,57 @@ class Comercio(Modulo):
   def __init__(self):
     self.base_datos = open("Comercio.txt","a")
     
- 
+class Administrativo (Modulo):
+
+  def __init__ (self):
+    self.base_datos = open("Empresas.txt", "b")
+
+
+  def acceder_Empresa (self, nombre):
+    for linea in self.base_datos:
+      datos = linea.split("|")
+
+      if (datos[1] == nombre):
+        self.empresa = Empresa ()   #Empresa con la que se va a trabajar
+
+
+  def crearEmpresa (self, nombre, tipo, nr_empleados, list_empleados, list_servicios, ofertas_laborales, codigo):
+    self.empresa = Empresa (nombre, tipo, nr_empleados, list_empleados, list_servicios, ofertas_laborales, codigo)                                                 #Empresa con la que se va a trabajar
+    self.base_datos.write(nombre + "|" + tipo + "|" + nr_empleados + "|" + list_empleados + "|" + list_servicios + "|" + ofertas_laborales + "|" + codigo)         #Registro en base de datos
+
+
+  def liquidar_Empresa (self, nombre):
+    temp = open ("temp_administrativo.txt", "w")  #Archivo temporal
+
+    #Escritura de todas las empresas menos la que se va a eliminar 
+    #en el archivo temporal
+    for linea in self.base_datos:
+      datos = linea.split()
+
+      if (datos[1] != nombre):
+        temp.write(linea)
+        
+        
+class Empresa:
+
+  def __init__ (self, nombre, tipo, nr_empleados, list_empleados, list_servicios, ofertas_laborales, codigo):
+    self.nombre = nombre                          
+    self.tipo = tipo                              
+    self.nr_empleados = nr_empleados              
+    self.list_empleados = list_empleados
+    self.list_servicios = list_servicios
+    self.ofertas_laborales = ofertas_laborales
+    self.codigo = codigo
+
+  def vender_prod ():
+    pass
+
+  def vender_serv ():
+    pass
+
+  def ofertas_lab ():
+    pass
+
+  def aceptar_trab ():
+    pass
+
